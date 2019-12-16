522b55671455afa2a3bffee26b9538c315d0ca93
Dmitrey <dmitrijp8@gmail.com>
2019-12-16 10:00:02 +0300

    intial commit

---
+++ src/main/java/com/popkov/iosu2/Iosu2Application.java
@@ -0,0 +1,13 @@
+package com.popkov.iosu2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Iosu2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Iosu2Application.class, args);
+    }
+
+}
