83552ec5bdc26af182e7b19cab714ba4936314be
Dmitrey <dmitrijp8@gmail.com>
2019-12-17 13:50:34 +0300

    test branch

---
+++ src/test/java/com/popkov/iosu2/Iosu2ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.popkov.iosu2;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Iosu2ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
